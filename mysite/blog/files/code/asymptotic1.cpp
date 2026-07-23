92b00207dee700da4902f7151a7d16df52cd2c70
0xSaad <75455552+Saad711T@users.noreply.github.com>
2026-07-23 07:02:28 +0300

    Added files

---
+++ mysite/blog/files/code/asymptotic1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+
+    int arr[1] = { 0 };
+    int arr[2] = { 0 };
+    int arr[3] = { 0 };
+
+
+    return 0;
+}
---
+++ mysite/blog/files/code/asymptotic2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+for(i=0;i<n;i++) {// c1
+   int arr[1] = {0}; //c2
+}
+
+
+    return 0;
+}
